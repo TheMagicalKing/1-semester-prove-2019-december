fcc829ca74dd559a5c2fbe0b85e9d293fe48b0c1
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-12-20 09:22:13 +0100

    Finished task three

--- src/Quickmaths.java
+++ src/Quickmaths.java
@@ -4,8 +4,47 @@
  * Licensed under the MIT License
  * 13/12/2019
  */
+import java.util.Scanner;
+
 
 public class Quickmaths {
+
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("User please enter number A");
+        int a=input.nextInt();
+        System.out.println("User please enter number B");
+        int b=input.nextInt();
+        System.out.println("This is the average of number A and B: " + gennemsnit(a,b) + " This is the sum of number A and B: "+ sum(a,b));
+        System.out.println("This part of the program keeps running as long as you enter numbers :)");
+        start();
+
+    }
+    public static int gennemsnit(int a, int b){
+                int c = a+b;
+                c=c/2;
+        return c;
+    }
+    public static int sum(int a, int b){
+        int c = a+b;
+        return c;
+    }
+    public static void start(){
+        Scanner input = new Scanner(System.in);
+        while (true){
+            int a,b,c;
+            System.out.println("User please input number A");
+            a = input.nextInt();
+            System.out.println("User please input number B");
+            b = input.nextInt();
+            c=a+b/2;
+            System.out.println("This is the average of A and B: "+c);
+            c=a+b;
+            System.out.println("This is the sum of A and B: "+c);
+        }
+    }
 
 
 }
--- test/QuickmathsTest.java
+++ test/QuickmathsTest.java
@@ -7,11 +7,21 @@ public class QuickmathsTest {
     @Test
     public void testSum() {
         // TODO Din kode her
+        Quickmaths sumTest = new Quickmaths();
+        assertEquals(17,sumTest.sum(10,7));
+        assertEquals(55, sumTest.sum(50,5));
+        assertEquals(100,sumTest.sum(50,50));
+        assertEquals(100,sumTest.sum(75,25));
     }
 
     @Test
     public void testGennemsnit() {
         // TODO Din kode her
+        Quickmaths averageTest = new Quickmaths();
+        assertEquals(75,averageTest.gennemsnit(100,50));
+        assertEquals(42,averageTest.gennemsnit(25,60));
+        assertEquals(68,averageTest.gennemsnit(75,62));
+        assertEquals(106,averageTest.gennemsnit(68,145));
     }
 
 }
